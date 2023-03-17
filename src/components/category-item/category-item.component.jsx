195a6ce35fbc82639069844365dad67fcbfd386b
RaulGoncalves21x <raulmago100@gmail.com>
2023-03-17 13:49:05 +0000

    Development of the Homepage. Two Components, and their respective styling created

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,18 @@
+import "./category-item.style.scss";
+
+const CategoryItem = ({ category }) => {
+  const {imageUrl, title} = category;
+  return (
+    <div className="category-item">
+      <div className="background-image" style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}/>
+      <div className="info">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem
